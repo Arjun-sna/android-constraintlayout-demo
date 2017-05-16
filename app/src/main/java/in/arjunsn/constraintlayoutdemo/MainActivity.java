1abcc1399948740115060051d784fc77b65f1209
Arjun <arjun@codebrahma.com>
2017-05-16 16:51:53 +0530

    dependencies added

--- app/src/main/java/in/arjunsn/constraintlayoutdemo/MainActivity.java
+++ app/src/main/java/in/arjunsn/constraintlayoutdemo/MainActivity.java
@@ -2,11 +2,21 @@ package in.arjunsn.constraintlayoutdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+  }
+
+  @OnClick(R.id.profile_page)
+  public void goToProfileView() {
+
   }
 }
