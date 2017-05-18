e5b06f91f5dcc9496144c7f970b75023bd8d50b7
Arjun <arjun@codebrahma.com>
2017-05-18 10:36:21 +0530

    constraint layout with scroll

---
+++ app/src/main/java/in/arjunsn/constraintlayoutdemo/ProfileViewScrollActivity.java
@@ -0,0 +1,16 @@
+package in.arjunsn.constraintlayoutdemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ProfileViewScrollActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_profile_view_scroll);
+  }
+}
